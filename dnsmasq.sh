3a34f138b7afe643bb4fbef24d1a08690e0979e9
currycan <ansandy@foxmail.com>
2018-10-11 16:19:13 +0800

    Update dnsmasq.sh

--- dnsmasq.sh
+++ dnsmasq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yum install dnsmasq -y
-echo "listen-address=127.0.0.1" >> /etc/dnsmasq.conf
+sed -i "669i listen-address=127.0.0.1" /etc/dnsmasq.conf
 sed -i "2i nameserver 127.0.0.1" /etc/resolv.conf
 systemctl enable dnsmasq
 systemctl start dnsmasq
