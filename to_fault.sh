c014be55b2c6e6471a183beab8061ba30dcdb825
currycan <ansandyzzt@gmail.com>
2019-03-26 16:37:35 +0800

    Update to_fault.sh

--- to_fault.sh
+++ to_fault.sh
@@ -2,5 +2,5 @@
 Date=$(date +%F" "%T) 
 HOST_NAME=$(hostname) 
 IP=$(ip addr | grep 192 | awk '/^[0-9]+: / {}; /inet.*global/ {print gensub(/(.*)\/(.*)/, "\\1", "g", $2)}') 
-Mail="zhangzhitao@fmsh.com.cn" 
+Mail="xxxxxx@fmsh.com.cn" 
 echo "$Date：主机$HOST_NAME切换为Fault，IP：$IP，请确认Nginx服务运行状态。"|mail -s "主机$HOST_NAME切换为Fault警告" $Mail 
